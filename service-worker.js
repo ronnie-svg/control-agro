6ac7f977047ce35726cac316cbc9dc4af905a671
ronnie-svg <izaskunarroiabe@gmail.com>
2026-06-04 12:28:41 +0200

    Improve service worker cache refresh

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "control-agro-v11";
+const cacheName = "control-agro-v12";
 const assets = [
   "./index.html",
   "./styles.css",
@@ -21,12 +21,32 @@ const assets = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(
+        keys
+          .filter((key) => key !== cacheName)
+          .map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
